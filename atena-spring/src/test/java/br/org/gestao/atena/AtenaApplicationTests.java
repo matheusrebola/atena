eeb5d383c50266597707dc1413abd5c0e0f15b8a
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:19:17 -0300

    first commit

---
+++ atena-spring/src/test/java/br/org/gestao/atena/AtenaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.gestao.atena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
