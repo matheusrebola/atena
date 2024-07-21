d74628b8b9ad40fd14e8fcfcc2b84cca81c9fd8f
matheusrebola <m.cavalcantir@outlook.com>
2024-07-21 16:30:53 -0300

    feat: configuração inicial das repositorys

--- atena-spring/src/main/java/br/org/gestao/atena/repositorys/IntegrationRepository.java
+++ atena-spring/src/main/java/br/org/gestao/atena/repositorys/IntegrationRepository.java
@@ -3,9 +3,11 @@ package br.org.gestao.atena.repositorys;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.org.gestao.atena.entities.Integrations;
 
+@Repository
 public interface IntegrationRepository extends JpaRepository<UUID, Integrations> {
 
 }
--- atena-spring/src/main/java/br/org/gestao/atena/repositorys/ProjectRepository.java
+++ atena-spring/src/main/java/br/org/gestao/atena/repositorys/ProjectRepository.java
@@ -3,9 +3,11 @@ package br.org.gestao.atena.repositorys;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.org.gestao.atena.entities.Projects;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<UUID, Projects> {
 
 }
